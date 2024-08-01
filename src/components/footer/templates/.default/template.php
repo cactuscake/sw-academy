bcd412ed9877f186be0a164a490b7d35d3dbf4a4
Evgeniy <cactuscake@mail.ru>
2024-08-01 10:19:44 +0500

    разбивает проект на компоненты задача:[12681]()

---
+++ src/components/footer/templates/.default/template.php
@@ -0,0 +1,26 @@
+<footer class="footer">
+        <div class="footer__container">
+            <div class="footer__logo--inner logo--inner">
+                <span class="footer__logo logo"><?= $arResult["foot_title"]?></span>
+                <p class="footer__logo--desc logo--desc"><?= $arResult["foot_subtitle"]?></p>
+            </div>
+            <div class="footer__block footer-menu">
+                <?php foreach ($arResult["menu"] as $menuItem) : ?>
+                    <a class="footer__block-text footer-menu-item" href="#"><?= $menuItem ?></a>
+                <?php endforeach;?>            
+            </div>
+            <div class="footer__block">
+                <p class="footer__block-title"><?= $arResult["contact_name"]?></p>
+                <?php foreach ($arResult["contact"] as $contactItem) : ?>
+                    <p class="footer__block-text"><?= $contactItem ?></a>
+                <?php endforeach;?>
+            </div>
+            <button class="button button__footer"><?= $arResult["foot_button"]?></button>
+        </div>
+        <hr class="footer__line">
+        <div class="footer__info">
+            <p class="footer__info-text"><?= $arResult["bottom"][0]?></p>
+            <p class="footer__info-text footer__info-text-orange"><?= $arResult["bottom"][1]?></p>
+            <p class="footer__info-text"><?= $arResult["bottom"][2]?></p>
+        </div>
+    </footer>
---
+++ src/components/header/component.php
@@ -0,0 +1,7 @@
+<?php
+
+$arResult["logo"] = $arParams["logo"];
+$arResult["desk"] = $arParams["desk"];
+$arResult["navbar"] = $arParams["navbar"];
+$arResult["number"] = $arParams["number"];
+$arResult["header_button"] = $arParams["header_button"];
